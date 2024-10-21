666cae670c9b09af97e36265fdbb1ed1986d6f93
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2024-10-21 22:41:15 +0800

    feat: impl "discrete_uniform" util

--- src/rlwe.rs
+++ src/rlwe.rs
@@ -11,3 +11,16 @@ fn discrete_gaussian(n: usize, q: i64, std_: f64) -> Rq {
     }
     Rq::new(coeffs, q)
 }
+
+fn discrete_uniform(n: usize, q: i64, min: Option<i64>, max: Option<i64>) -> Rq {
+    let mut rng = rand::thread_rng();
+
+    let min = min.unwrap_or(0);
+    let max = max.unwrap_or(q);
+
+    let mut coeffs = vec![0; n];
+    for i in 0..n {
+        coeffs[i] = rng.gen_range(min..=max);
+    }
+    Rq::new(coeffs, q)
+}
