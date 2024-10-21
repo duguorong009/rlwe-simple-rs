768b4e30bf1c118c9be06eee619e71e7ee3111dd
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2024-10-21 22:37:26 +0800

    feat: impl "discrete_gaussian" util

--- src/rlwe.rs
+++ src/rlwe.rs
@@ -1,1 +1,13 @@
+use rand::Rng;
 
+use crate::rq::Rq;
+
+
+fn discrete_gaussian(n: usize, q: i64, std_: f64) -> Rq {
+    let mut rng = rand::thread_rng();
+    let mut coeffs = vec![0; n];
+    for i in 0..n {
+        coeffs[i] = (rng.gen::<f64>() * std_).round() as i64;
+    }
+    Rq::new(coeffs, q)
+}
